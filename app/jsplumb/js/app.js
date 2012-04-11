bd1c38a2b5d0cf45c49650016104373b4359899c
Hans Christian Reinl <anything@drublic.de>
2012-04-11 21:48:55 +0200

    Move controller into own file

--- app/jsplumb/js/app.js
+++ app/jsplumb/js/app.js
@@ -42,46 +42,6 @@
 
 
 
-	Modeller.modellerController = Ember.ArrayProxy.create({
-	  content: [],
-
-
-	  createModell: function (title, desc) {
-	    var modell = Modeller.Modell.create({
-	    	title : title,
-	    	desc : desc
-	   	});
-	    this.pushObject(modell);
-	    return Modeller.Storage.create(modell);
-	  },
-
-
-	  createConnection: function (source, target, title) {
-	    var connection = Modeller.Connection.create({
-	    	source : source,
-	    	target : target,
-	    	label : {
-	    		title : title
-	    	}
-	   	});
-	    this.pushObject(connection);
-	    return Modeller.Storage.create(connection);
-	  },
-
-	  pushObject: function (item, ignoreStorage) {
-	    if (!ignoreStorage)
-	      Modeller.Storage.create(item);
-	    return this._super(item);
-	  },
-
-	  removeObject: function (item) {
-	    Modeller.Storage.remove(item);
-	    return this._super(item);
-	  }
-	});
-
-
-
 
 
 
