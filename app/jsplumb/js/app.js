7df0a32610788bd3738c06b63865115b458f93c6
Hans Christian Reinl <anything@drublic.de>
2012-04-11 21:18:50 +0200

    Move dialogue stuff in own file

--- app/jsplumb/js/app.js
+++ app/jsplumb/js/app.js
@@ -19,34 +19,6 @@
 
 
 
-	var	dialogue = function () {
-		var view = Modeller.dialogueView.create({
-			title : "Add Component",
-			classNames : ['dialogue', 'dialogue-add']
-		});
-
-		return {
-			add : function () {
-				var template, output;
-
-				$('body').append('<div class="overlay"></div>');
-
-				// Append it to the body
-				view.append();
-			},
-
-			close : function () {
-				view.remove();
-				$('.dialogue').remove();
-				$('.overlay').remove();
-			}
-		}
-	};
-
-
-
-
-
 
 	// Create an Modell object
 	Modeller.Modell = Ember.Object.extend({
@@ -207,76 +179,6 @@
 		// Rendering for views
 		Modeller.modellerView.appendTo('.main');
 	} ();
-
-
-	// Events
-	$(document)
-
-		// When clicking to add a component
-		.on('click', '.add-component', function (e) {
-			e.preventDefault();
-			dialogue().add();
-		})
-
-		// Dialogue close
-		.on('click', '.dialogue-close', function () {
-			dialogue().close();
-		})
-
-		// Overlay
-		.on('click', '.overlay', function () {
-			dialogue().close();
-		})
-
-		// Esc
-		.on('keydown', function (e) {
-			e.keyCode === 27 && dialogue().close();
-		})
-
-
-		// Submitting form
-		.on('submit', '.dialogue-add', function (e) {
-			e.preventDefault();
-
-			var new_modell,
-					isComplete = true,
-					values = {},
-					$fields = $(this).find('input, textarea, select');
-			
-			$fields.each(function () {
-				if ($(this).attr('id')) {
-					values[$(this).attr('id')] = $(this).val();
-
-					if (!$(this).val()) {
-						isComplete = false;
-					}
-				}
-			});
-
-			if (!isComplete) {
-				return false;
-			}
-
-			// Set a new object
-			new_modell = Modeller.modellerController.createModell(values['modell-title'], values['modell-desc']);
-
-			// Set connections?
-			if (values['modell-relation']) {
-				Modeller.modellerController.createConnection(new_modell.id, values['modell-relation'], values['connection-title']);
-			}
-
-			dialogue().close();
-		})
-
-
-
-
-
-
-
-
-
-		;
 
 
 
