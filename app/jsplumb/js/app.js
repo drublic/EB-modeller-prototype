78ec799d67a290341ed90913e6a9bd95081d2fc0
Hans Christian Reinl <anything@drublic.de>
2012-04-11 22:21:29 +0200

    Remove comment form logger

--- app/jsplumb/js/app.js
+++ app/jsplumb/js/app.js
@@ -1,74 +1,65 @@
 
-// IIFE fo fastr XS
-+ function ($, exports, undefined) {
-
-	
-	var _plumber = {
-		colors : {
-			connector_stroke : "rgba(100, 100, 100, 1)",
-			connector_stroke_highlight : "rgba(200, 200, 200, 1)",
-			outline : "rgba(50, 50, 50, 1)",
-			hover_paint : {
-				 strokeStyle: "#7ec3d9"
-			},
-			endpoint : {
-				fillStyle: "#a7b04b"
-			}
+var _plumber = {
+	colors : {
+		connector_stroke : "rgba(100, 100, 100, 1)",
+		connector_stroke_highlight : "rgba(200, 200, 200, 1)",
+		outline : "rgba(50, 50, 50, 1)",
+		hover_paint : {
+			 strokeStyle: "#7ec3d9"
+		},
+		endpoint : {
+			fillStyle: "#a7b04b"
 		}
-	};
+	}
+};
 
 
 
-	// View for Modells
-	Modeller.modellerView = Ember.View.create({
-		templateName : 'components',
-		modells : Modeller.Storage.findAllByType(),
+// View for Modells
+Modeller.modellerView = Ember.View.create({
+	templateName : 'components',
+	modells : Modeller.Storage.findAllByType(),
 
-		// After the elements are inserted
-		didInsertElement : function () {
+	// After the elements are inserted
+	didInsertElement : function () {
 
-			// Find all connections in storage
-			var connections = Modeller.Storage.findAllByType("Connection");
+		// Find all connections in storage
+		var connections = Modeller.Storage.findAllByType("Connection");
 
-			// make components draggable
-			jsPlumb.draggable( $('.component') );
+		// make components draggable
+		jsPlumb.draggable( $('.component') );
 
-			// Render Connections
-			connections.forEach(function (item) {
-				item.render();
-			});
-		}
-	});
-
-
+		// Render Connections
+		connections.forEach(function (item) {
+			item.render();
+		});
+	}
+});
 
 
 
 
-	// Init Modells
-	+ function () {
-		var items = Modeller.Storage.findAllByType();
+// Init Modells
++ function () {
+	var items = Modeller.Storage.findAllByType();
 
-		// If items is empty, add two components
-		if (items.length < 1) {
-			var modell_1, modell_2;
+	// If items is empty, add two components
+	if (items.length < 1) {
+		var modell_1, modell_2;
 
-			modell_1 = Modeller.modellerController.createModell("Window 1", "I am plumbed with a Bezier connector to Window 2 and a label");
-			modell_2 = Modeller.modellerController.createModell("Window 2", "I am plumbed with a Bezier connector to Window 1.");
+		modell_1 = Modeller.modellerController.createModell("Window 1", "I am plumbed with a Bezier connector to Window 2 and a label");
+		modell_2 = Modeller.modellerController.createModell("Window 2", "I am plumbed with a Bezier connector to Window 1.");
 
-			Modeller.modellerController.createConnection(modell_1.id, modell_2.id, "A Connection");
+		Modeller.modellerController.createConnection(modell_1.id, modell_2.id, "A Connection");
 
-			items = Modeller.Storage.findAllByType();
-		}
+		items = Modeller.Storage.findAllByType();
+	}
 
-		// Save all that stuff in modellerController
-		Modeller.modellerController.set('[]', items);
+	// Save all that stuff in modellerController
+	Modeller.modellerController.set('[]', items);
 
-		// Rendering for views
-		Modeller.modellerView.appendTo('.main');
-	} ();
+	// Rendering for views
+	Modeller.modellerView.appendTo('.main');
+} ();
 
-
-
-} (jQuery, window);
 
